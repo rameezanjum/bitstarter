001636c4fa9737d5fce2350ff4d851d00611a73c
Rameez Anjum <rameez_171@hotmail.com>
2013-07-06 22:23:52 -0400

    did not use buffer this time

--- web.js
+++ web.js
@@ -4,10 +4,11 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
- var buffer = new Buffer(100); 
- buffer.write(fs.readFileSync("index.html","utf8"),"utf8");
+// var buffer = new Buffer(10000); 
+// buffer.write(fs.readFileSync("index.html","utf8"),"utf8");
  
- response.send(buffer.toString("utf8", 0, 28));
+// response.send(buffer.toString("utf8", 0, 28));
+response.send(fs.readFileSync("index.html","utf8"));
 });
 
 var port = process.env.PORT || 5000;
