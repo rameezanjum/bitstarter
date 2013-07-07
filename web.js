331070896ae6812180fc90981a904219cd6704c1
Rameez Anjum <rameez_171@hotmail.com>
2013-07-06 21:16:37 -0400

    Read from file, write to buffer and print on screen

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ var buffer = new Buffer(100); 
+ buffer.write(fs.readFileSync("index.html","utf8"),"utf8");
+ 
+ response.send(buffer.toString("utf8", 0, 28));
 });
 
 var port = process.env.PORT || 5000;
